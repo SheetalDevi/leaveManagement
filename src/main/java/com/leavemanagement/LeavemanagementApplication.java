57364fba96c8e7177e9c957cde7f41a47b9f4be0
SheetalDevi <sheetal.devi@anviam.com>
2022-11-24 16:15:33 +0530

    update

--- src/main/java/com/leavemanagement/LeavemanagementApplication.java
+++ src/main/java/com/leavemanagement/LeavemanagementApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /* This project use for leave apply
  * @author Akash kumar
+ * @Sheetal
 */
 @SpringBootApplication
 public class LeavemanagementApplication{
-
-	
 //  http://localhost:9000/leave-management/swagger-ui.html   --Swagger URL
 	public static void main(String[] args) {
 		SpringApplication.run(LeavemanagementApplication.class, args);
